2ecdbfa294b0e9ea8c9b9681d562c90367d65f4d
kjhonggg95 <kjhonggg95@gmail.com>
2023-11-09 13:14:38 +0900

    프로젝트 세팅

---
+++ tierup/src/main/java/com/tierup/tierup/TierupApplication.java
@@ -0,0 +1,13 @@
+package com.tierup.tierup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TierupApplication.class, args);
+	}
+
+}
---
+++ tierup/src/test/java/com/tierup/tierup/TierupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tierup.tierup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
