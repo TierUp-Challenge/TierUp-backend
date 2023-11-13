2d4f57fbd90d02c0a2e4df62781a91fce2c0f787
이용재 <80679257+dydwo0740@users.noreply.github.com>
2023-11-13 21:23:49 +0900

    feat: orElseThrow를 활용한 코드 래픽토링

--- tierup/src/main/java/com/tierup/tierup/challenge/service/ChallengeService.java
+++ tierup/src/main/java/com/tierup/tierup/challenge/service/ChallengeService.java
@@ -31,12 +31,9 @@ public class ChallengeService {
 
     public ChallengeDto findChallengeById(Long id) {
         Optional<Challenge> challengeOptional = repository.findById(id);
-        if (challengeOptional.isEmpty()) {
-            // 오류 처리
-            throw new RuntimeException("Challenge not found with id: " + id);
-        }
 
-        Challenge challenge = challengeOptional.get();
+        Challenge challenge = challengeOptional.orElseThrow(()
+                -> new RuntimeException("Challenge not found with id: " + id));
 
         ChallengeDto challengeDto = ChallengeDto.builder()
                 .name(challenge.getName())
