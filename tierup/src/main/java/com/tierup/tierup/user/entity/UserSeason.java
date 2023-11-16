877c74d0cd094c21567bf020dd57038c0c217ba4
이용재 <80679257+dydwo0740@users.noreply.github.com>
2023-11-16 10:34:09 +0900

    feat: UserChallenge 에서 UserSeason으로 이름 변경

--- tierup/src/main/java/com/tierup/tierup/user/entity/UserSeason.java
+++ tierup/src/main/java/com/tierup/tierup/user/entity/UserSeason.java
@@ -1,6 +1,6 @@
 package com.tierup.tierup.user.entity;
 
-import com.tierup.tierup.challenge.entity.Challenge;
+import com.tierup.tierup.season.entity.Season;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,16 +9,18 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserChallenge {
+public class UserSeason {
     @Id @GeneratedValue
+    @Column(name = "user_season_id")
     private Long id;
-    private Long challengePoint;
+    @Column(name = "user_season_point")
+    private Long seasonPoint;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "challenge_id")
-    private Challenge challenge;
+    @JoinColumn(name = "season_id")
+    private Season season;
 }
